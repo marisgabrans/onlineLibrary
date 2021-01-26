4ab6a6302a047aee4e4cf8d3f520c15b21334356
Maris Gabrans <maris_gabrans@yahoo.com>
2021-01-26 13:00:57 +0300

    Project Skeleton

--- src/main/java/com/example/onlinelibrary/model/Book.java
+++ src/main/java/com/example/onlinelibrary/model/Book.java
@@ -1,9 +1,11 @@
 package com.example.onlinelibrary.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Table(name = "books")
 @Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
