996719a8bb16087cc5422a04446dff48577724fa
Maris Gabrans <maris_gabrans@yahoo.com>
2021-01-26 16:06:41 +0300

    First running version with html (now really running)

--- src/main/java/com/example/onlinelibrary/controller/BookController.java
+++ src/main/java/com/example/onlinelibrary/controller/BookController.java
@@ -3,11 +3,13 @@ package com.example.onlinelibrary.controller;
 import com.example.onlinelibrary.model.Book;
 import com.example.onlinelibrary.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Controller
 public class BookController {
     private final BookService bookService;
 
--- src/main/java/com/example/onlinelibrary/model/Book.java
+++ src/main/java/com/example/onlinelibrary/model/Book.java
@@ -2,8 +2,8 @@ package com.example.onlinelibrary.model;
 
 import javax.persistence.*;
 
-@Table(name = "books")
 @Entity
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
