0e06fb3c85df4303a6dcb97b2967e7b228df3748
Maris Gabrans <maris_gabrans@yahoo.com>
2021-01-26 11:49:42 +0300

    test

--- src/main/java/com/example/onlinelibrary/OnlineLibraryApplication.java
+++ src/main/java/com/example/onlinelibrary/OnlineLibraryApplication.java
@@ -8,6 +8,7 @@ public class OnlineLibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineLibraryApplication.class, args);
+		System.out.println("Test string");
 	}
 
 }
