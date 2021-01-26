f34041b288c4865b2315ce221d9960458f203e6a
Maris Gabrans <maris_gabrans@yahoo.com>
2021-01-26 12:11:17 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/onlinelibrary/OnlineLibraryApplication.java
+++ src/main/java/com/example/onlinelibrary/OnlineLibraryApplication.java
@@ -9,6 +9,7 @@ public class OnlineLibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineLibraryApplication.class, args);
 		System.out.println("Test string");
+		System.out.println("second test");
 	}
 
 }
