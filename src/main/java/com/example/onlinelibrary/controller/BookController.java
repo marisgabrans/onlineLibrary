addc877e55398a574f26a10895c694b44135f6de
raynova <reynnia@gmail.com>
2021-01-27 20:37:55 +0200

    dropdown search menu fix

--- src/main/java/com/example/onlinelibrary/controller/BookController.java
+++ src/main/java/com/example/onlinelibrary/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.example.onlinelibrary.controller;
 
 import com.example.onlinelibrary.model.Book;
+import com.example.onlinelibrary.model.Genre;
+import com.example.onlinelibrary.repository.GenreRepository;
 import com.example.onlinelibrary.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,16 +16,20 @@ import java.util.List;
 @Controller
 public class BookController {
     private final BookService bookService;
+    private final GenreRepository genreRepository;
 
     @Autowired
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, GenreRepository genreRepository) {
         this.bookService = bookService;
+        this.genreRepository = genreRepository;
     }
 
     @GetMapping("/books")
     public String findAll(Model model) {
         List<Book> books = bookService.findAll();
+        List<Genre> genres = genreRepository.findAll();
         model.addAttribute("books", books);
+        model.addAttribute("genres", genres);
         return "/books-list";
     }
 
