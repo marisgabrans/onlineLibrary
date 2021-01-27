c2b2150adf0282bbbaea7ad5bc0b269c38c0a9b1
Guntars Kirovainis <guntars.kirovainis@gmail.com>
2021-01-27 15:31:31 +0200

    Small changes to Config/WebSecurityConfig

--- src/main/java/com/example/onlinelibrary/controller/UserController.java
+++ src/main/java/com/example/onlinelibrary/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.onlinelibrary.controller;
 
 import com.example.onlinelibrary.model.User;
 import com.example.onlinelibrary.service.UserService;
+import com.example.onlinelibrary.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,19 +14,19 @@ import java.util.List;
  * Currently not used. TODO - add @Get and @Post mappings for different actions with users (User administration page)
  */
 
-//@Controller
-//public class UserController {
-//    private final UserService userService;
-//
-//    @Autowired
-//    public UserController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping("/users")
-//    public String findAll(Model model) {
-//        List<User> users = userService.findAll();
-//        model.addAttribute("users", users);
-//        return "/users-list";
-//    }
-//}
+@Controller
+public class UserController {
+    private final UserServiceImpl userServiceImpl;
+
+    @Autowired
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model) {
+        List<User> users = userServiceImpl.findAll();
+        model.addAttribute("users", users);
+        return "/users-list";
+    }
+}
