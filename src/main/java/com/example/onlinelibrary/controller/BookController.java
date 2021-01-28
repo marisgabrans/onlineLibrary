a215fe05aa0670d63d8c8ede86fd8538c2d84e57
Maris Gabrans <maris_gabrans@yahoo.com>
2021-01-28 17:19:52 +0300

    update page and method

--- src/main/java/com/example/onlinelibrary/controller/BookController.java
+++ src/main/java/com/example/onlinelibrary/controller/BookController.java
@@ -9,10 +9,13 @@ import com.example.onlinelibrary.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -61,5 +64,24 @@ public class BookController {
         Book book = bookService.findById(book_id);
         model.addAttribute("book", book);
         return "/book-page";
+    }
+
+    @GetMapping("update/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Book book = bookService.findById(id);
+        model.addAttribute("book", book);
+        return "book-update";
+    }
+
+    @PostMapping("update/{id}")
+    public String updateBook(@PathVariable("id") long id, @Valid Book book, BindingResult result,
+                                Model model) {
+        if (result.hasErrors()) {
+            return "book-update";
+        }
+
+        bookService.saveBook(book);
+        model.addAttribute("students", bookService.findAll());
+        return "index";
     }
 }
