8303e615b2780387413f0669c781bf9f6523cdb7
Eriks Cuhrukidze <eriks.cuhrukidze@gmail.com>
2021-01-29 14:03:16 +0200

    removing unused class import

--- src/main/java/com/example/onlinelibrary/repository/BookRepository.java
+++ src/main/java/com/example/onlinelibrary/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.example.onlinelibrary.repository;
 
 import com.example.onlinelibrary.model.Book;
-import com.example.onlinelibrary.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
