233b49e8d7a64bf28b07bdb6032330f1723635d9
Guntars Kirovainis <guntars.kirovainis@gmail.com>
2021-01-30 12:19:42 +0200

    Fixed book list filter so that it does the search also by genres and authors

--- src/main/java/com/example/onlinelibrary/repository/BookRepository.java
+++ src/main/java/com/example/onlinelibrary/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.example.onlinelibrary.repository;
 import com.example.onlinelibrary.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,14 +11,13 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findBookById(Long id);
 
-    @Query("SELECT b FROM Book b WHERE b.title LIKE %?1%"
-            + " OR b.description LIKE %?1%")
-//            + " OR b.genre LIKE %?1%"
-//            + " OR CONCAT(p.price, '') LIKE %?1%")
-    List<Book> search(String keyword);
-
-
-//    @Query("SELECT p FROM Product p WHERE CONCAT(p.name, p.brand, p.madein, p.price) LIKE %?1%")
-//    public List<Product> search(String keyword);
+    @Query(value = "SELECT * FROM books b "
+            + "JOIN authors a "
+            + "ON b.author_id = a.author_id "
+            + "JOIN genre g "
+            + "ON b.genre_id = g.genre_id "
+            + "WHERE CONCAT(title, ' ', first_name, ' ', last_name, ' ', genre_name, ' ', description) "
+            + "LIKE %:keyword%", nativeQuery = true)
+    List<Book> search(@Param("keyword") String keyword);
 
 }
