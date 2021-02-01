5c32397459f898aa157513138b5346a05640caa8
Eriks Cuhrukidze <eriks.cuhrukidze@gmail.com>
2021-02-01 14:50:40 +0200

    email test

--- src/main/java/com/example/onlinelibrary/service/BookService.java
+++ src/main/java/com/example/onlinelibrary/service/BookService.java
@@ -21,104 +21,104 @@ import java.util.List;
 import java.util.Random;
 
 @Service
-    public class BookService {
-        private final BookRepository bookRepository;
+public class BookService {
+    private final BookRepository bookRepository;
 
-        @Autowired
-        private JavaMailSender emailSender;
+    @Autowired
+    private JavaMailSender emailSender;
 
-        @Autowired
-        public BookService(BookRepository bookRepository) {
-            this.bookRepository = bookRepository;
-        }
-
-        public Book findById(Long id) {
-            return bookRepository.getOne(id);
-        }
-
-        public List<Book> findAll( ) {
-            return bookRepository.findAll();
-        }
-
-        public Book saveBook(Book book) {
-            return bookRepository.save(book);
-        }
-
-        public void deleteById(Long id) {
-            bookRepository.deleteById(id);
-        }
-
-        public void deleteBook(Book book) {
-            bookRepository.delete(book);
-        }
-
-        public Book updateBook(Book bookDetails, Long id) {
-            Book book = bookRepository.findBookById(id);
-            book.setTitle(bookDetails.getTitle());
-            book.setAuthor(bookDetails.getAuthor());
-            book.setTitle(bookDetails.getTitle());
-            book.setGenre(bookDetails.getGenre());
-            book.setPages(bookDetails.getPages());
-            book.setDescription(bookDetails.getDescription());
-            book.setQuantity(bookDetails.getQuantity());
-            book.setCover(bookDetails.getCover());
-            return bookRepository.save(book);
-        }
-
-
-        public boolean reservation(Book book) {
-            Integer quantity = book.getQuantity();
-            if (quantity <= 0) {
-                return false;
-            } else {
-                quantity = quantity - 1;
-                book.setQuantity(quantity);
-                bookRepository.save(book);
-
-                Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-                String username = null;
-                if (principal instanceof UserDetails) {
-                    username = ((UserDetails)principal).getUsername();
-                } else {
-                    username = principal.toString();
-                }
-
-                Random rand = new Random();
-                // Generating random integers in range 0 to 9999
-                int int1 = rand.nextInt(10000);
-
-                sendSimpleMessage(username, "Your reservation confirmed",
-                        "Dear customer," + "\n\nThank you for choosing Online Library. Your request number is #" + int1 + "!" +
-                                "\nWe are pleased to confirm your reservation. You have 24 hours to pick up the selected book." + "\n\nBook pick-up address: Krisjana Barona iela 14, Riga, LV-1050" +
-                        "\nOpening hours - I-V: 08:00 - 20:00" + "\nContacts: phone - +371 26899876; e-mail: onlinelibraryteam@gmail.com" + "\n\n\n\n\nBest regards," + "\nOnlineLibrary administration.");
-                return true;
-            }
-        }
-
-
-
-        private void sendSimpleMessage(
-                String to, String subject, String text) {
-
-
-
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("onlinelibraryteam@gmail.com");
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(text);
-            emailSender.send(message);
-        }
-
-
-
-
-        public List<Book> search(String keyword) {
-            if (keyword != null) {
-                return bookRepository.search(keyword);
-            }
-            return bookRepository.findAll();
-        }
-
-
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
+
+    public Book findById(Long id) {
+        return bookRepository.getOne(id);
+    }
+
+    public List<Book> findAll( ) {
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    public void deleteBook(Book book) {
+        bookRepository.delete(book);
+    }
+
+    public Book updateBook(Book bookDetails, Long id) {
+        Book book = bookRepository.findBookById(id);
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        book.setTitle(bookDetails.getTitle());
+        book.setGenre(bookDetails.getGenre());
+        book.setPages(bookDetails.getPages());
+        book.setDescription(bookDetails.getDescription());
+        book.setQuantity(bookDetails.getQuantity());
+        book.setCover(bookDetails.getCover());
+        return bookRepository.save(book);
+    }
+
+
+    public boolean reservation(Book book) {
+        Integer quantity = book.getQuantity();
+        if (quantity <= 0) {
+            return false;
+        } else {
+            quantity = quantity - 1;
+            book.setQuantity(quantity);
+            bookRepository.save(book);
+
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            String username = null;
+            if (principal instanceof UserDetails) {
+                username = ((UserDetails)principal).getUsername();
+            } else {
+                username = principal.toString();
+            }
+
+            Random rand = new Random();
+            // Generating random integers in range 0 to 9999
+            int int1 = rand.nextInt(10000);
+
+            sendSimpleMessage(username, "Your reservation confirmed",
+                    "Dear customer," + "\n\nThank you for choosing Online Library. Your request number is #" + int1 + "!" +
+                            "\nWe are pleased to confirm your reservation. You have 24 hours to pick up the selected book." + "\n\nBook pick-up address: Krisjana Barona iela 14, Riga, LV-1050" +
+                            "\nOpening hours - I-V: 08:00 - 20:00" + "\nContacts: phone - +371 26899876; e-mail: onlinelibraryteam@gmail.com" + "\n\n\n\n\nBest regards," + "\nOnlineLibrary administration.");
+            return true;
+        }
+    }
+
+
+
+    private void sendSimpleMessage(
+            String to, String subject, String text) {
+
+
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("onlinelibraryteam@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+
+
+
+    public List<Book> search(String keyword) {
+        if (keyword != null) {
+            return bookRepository.search(keyword);
+        }
+        return bookRepository.findAll();
+    }
+
+
+}
