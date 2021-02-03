bc0dd8d58804528ed102cd6d4bb153f2feffaacc
Eriks Cuhrukidze <eriks.cuhrukidze@gmail.com>
2021-02-03 19:52:56 +0200

    email configs

--- src/main/java/com/example/onlinelibrary/service/BookService.java
+++ src/main/java/com/example/onlinelibrary/service/BookService.java
@@ -100,7 +100,7 @@ public class BookService {
 
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("onlinelibraryteam@gmail.com");
+        message.setFrom("worms.boook@gmail.com");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
